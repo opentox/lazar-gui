81a12c5206b6adf25f41c2eb104e25db457ebd78
gebele <gebele@in-silico.ch>
2019-09-20 10:39:33 +0000

    automatic server url

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,7 @@
 # route to swagger API file
 get "/api/api.json" do
+  api_file = File.join("api", "api.json")
+  `sed -i 's|SERVER_URI|#{request.base_url}|g' #{api_file}`
   response['Content-Type'] = "application/json"
   api_file = File.join("api", "api.json")
   bad_request_error "API Documentation in Swagger JSON is not implemented." unless File.exists?(api_file)
