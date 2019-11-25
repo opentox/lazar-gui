d9c4c63ac7393857b768747033cdec9ce2d1313c
gebele <gebele@in-silico.ch>
2019-11-25 14:34:57 +0000

    remove api host

--- lib/api.rb
+++ lib/api.rb
@@ -6,6 +6,5 @@ get "/api/api.json" do
   api_file = File.join("api", "api.json")
   bad_request_error "API Documentation in Swagger JSON is not implemented." unless File.exists?(api_file)
   api_hash = JSON.parse(File.read(api_file))
-  api_hash["host"] = request.env['HTTP_HOST']
   return api_hash.to_json
 end
